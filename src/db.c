7a1dd411625c41e580c64eee6592df7f6a4e0398
Bioteq <mike@nanobit.net>
2021-05-03 19:10:59 -0500

    src push

---
+++ src/db.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include <winsock.h>
+#include <richedit.h>
+#include <assert.h>
+#include <time.h>
+#include <commctrl.h>
+#include "Nano PoS.h"
+#include "NWC.h"
+
+
+
+
+void create_db(void)
+{
+
+}
+
+void close_db(void)
+{
+
+
+}
+
+BOOL is_db_locked(FILE * db)
+{
+	return FALSE; // Not locked
+}
+
+BOOL lock_db(FILE* fp)
+{
+	return FALSE;
+	
+}
+
+BOOL unlock_db(FILE* fp)
+{
+	return FALSE;
+
+}
+
+int write_db(FILE* fp, char* stmt)
+{
+	return 0;
+}
+
