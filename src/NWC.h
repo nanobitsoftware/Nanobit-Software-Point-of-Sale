299f63134dc7bbc32641aafd87f92ecf339eebe8
bioteq <mike@nanobit.net>
2022-01-12 14:18:49 -0600

    Merge rebase

--- src/NWC.h
+++ src/NWC.h
@@ -1,6 +1,9 @@
+
+#define NWC_MAX_GUI 2048 // Max total items; windows, controls, children, etc.
+
 #define MAX_CHILDREN 1024
 #define MAX_CONTROLS 512
-
+#define MAX_WIDGETS MAX_CONTROLS // To bring nomenclature in to line for the changes.
 #define BUTTON     1
 #define RADIO      2
 #define VSCROLL    3
@@ -16,46 +19,67 @@ typedef struct NWC_Parent NWC_PARENT;
 typedef struct NWC_Child  NWC_CHILD;
 typedef struct NWC_Child_attr NWC_CHILD_ATTR;
 typedef struct window_controls NWC_CTRL;
+typedef struct window_controls NWC_WIDGET; // Widgets are the new name for controls to be more in line with other libraries.
+typedef struct NWC_Parent NWC_WINDOW; // Cleaning up our nomenclature.
 
-NWC_PARENT** parents;
-int current_parents;			    
+NWC_PARENT** parents; // List of parent windows.
+#define nwc_window_list  parents // Just an alias for parents. To make nomenclature easier with the changes to make it easier to read. Will be moot after all changes.
+int current_parents;
 int max_parents;
+int current_gui; // Total windows, children, widgets, etc. 
+unsigned long int nwc_memory_used;
+
+
 
 struct NWC_Parent
 {
-	char* name;
+	BOOL on_top;
+	BOOL created;
+
 	int		children;
 	int		index;
 	int width;
 	int heigth;
 	int x;
 	int y;
-	BOOL on_top;
-	BOOL created;
-	HWND window_control;
-	HWND window_pointer;
-	HINSTANCE instance;
-	LRESULT* control_proc;
-	unsigned long int window_options;
-	unsigned long int style_options;
-	void  (*create_child)(void*, ...);
-	void* destroy_child;
-	void* destroy_self;
-	NWC_CTRL** controls;
+
 	int control_count;
 	int max_controls;
+
+	char* name;
+
+	void  (*create_child)(void*, ...);
+	void* create_widget;
+	void* destroy_child;
+	void* destroy_self;
+
+	unsigned long int window_options;
+	unsigned long int style_options;
+
+	HWND window_control;
+	HWND window_pointer;
+
+	HINSTANCE instance;
+	LRESULT* control_proc;
+
+	NWC_WIDGET** controls;
 	NWC_CHILD* child;
+	NWC_PARENT* self;
 };
 
 struct NWC_Child
 {
 	char* name;
-	NWC_PARENT* parent;
-	void** controls;
-	NWC_CHILD_ATTR** controls_attr;
+
 	int		index;
-	LRESULT* control_proc;
+
+	void** controls;
+
 	NWC_CHILD* next;
+	NWC_PARENT* parent;
+
+	NWC_CHILD_ATTR** controls_attr;
+	LRESULT* control_proc;
 };
 
 struct NWC_Child_attr
@@ -72,8 +96,6 @@ struct window_controls
 {
 	char* name;
 	int    type;
-	HWND   handle;
-	NWC_PARENT* parent;
 	int x;
 	int y;
 	int height;
@@ -82,7 +104,12 @@ struct window_controls
 	int clist_index;
 	DWORD style;
 	DWORD  id;
+	HWND   handle;
+	NWC_PARENT* parent;
+
 };
+
+
 
 NWC_CHILD* create_child(NWC_PARENT* p_window, char* name);
 BOOL set_parent_config(NWC_PARENT* p_window, HWND hwnd, LRESULT* proc, int x, int y, int width, int heigth, HINSTANCE instance, BOOL on_top, unsigned long int window_options, unsigned long int style_options);
@@ -141,3 +168,9 @@ void parent_disable_all(NWC_PARENT* p_window);
 int get_max_control_heigth(NWC_PARENT* p);
 int get_max_control_width(NWC_PARENT* p);
 NWC_CTRL* get_control_by_id(NWC_PARENT* p_window, DWORD id);
+
+void* nwc_widget_create_entry(NWC_PARENT* self);
+
+void* check_self(void* func);
+void push_self(void* func, void* self);
+void pop_self(void* func, void* self);
